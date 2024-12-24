c030556b839081c3a9b86be9d9c710d6ba12acf4
LAKSHMI VENKATA SATYA GANESH <22p5a0510@vishnu.edu.in>
2024-12-24 14:33:28 +0530

    second commit

---
+++ client/src/baseURL.js
@@ -0,0 +1,3 @@
+const baseURL = 'http://arshchats-cfd8decgfgc7a7d9.eastus-01.azurewebsites.net';
+
+export default baseURL;
--- server/index.js
+++ server/index.js
@@ -48,4 +48,9 @@ io.on('connect', (socket) => {
   })
 });
 
-server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
+app.use(express.static('./client/build'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+
+server.listen(process.env.POR || 5000, () => console.log(`Server has started.`));
